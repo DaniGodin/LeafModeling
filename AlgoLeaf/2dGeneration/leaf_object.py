27cfaafbb69aafe3439f17b9602e646aa3e7f06b
Daniel Godin <danielgodin.pro@gmail.com>
2019-03-07 00:25:12 +0100

    [Leaf] reorganised the objects inside this module

--- AlgoLeaf/2dGeneration/leaf_object.py
+++ AlgoLeaf/2dGeneration/leaf_object.py
@@ -9,6 +9,7 @@ from matplotlib import collections as mc
 import matplotlib.pyplot as plt
 from matplotlib.patches import Ellipse
 
+
 class Leaf:
     """
     This object decribe the leaf shape
@@ -47,73 +48,6 @@ class Leaf:
         self.shape.set_alpha(0.1)
         self.shape.set_facecolor("Green")
         self.ax.add_artist(self.shape)
-
-
-
-
-
-
-
-class Particle_set:
-    """
-    This object represent a set of Partciles object.
-    """
-
-    particles = []
-    petiole = None
-
-    def __init__(self, particles, petiole):
-        self.particles = particles
-        self.petiole = petiole
-        self.init_venation()
-        self.init_vector()
-
-
-    def display_particles(self):
-         for p in self.particles:
-            p.display_particle()
-
-    def init_venation(self):
-        for p in self.particles:
-            p.last_venation = VenationPoint(p.coord, [])
-
-
-    def init_vector(self):
-        for p in self.particles:
-            p.dir_to_p = [self.petiole[0] - p.coord[0], self.petiole[1] - p.coord[1]]
-
-    def move_particles(self):
-        for p in self.particles:
-            p.move()
-        self.init_vector()
-
-    def get_venations(self):
-        venations = []
-        for p in self.particles:
-            venations.append(p.last_venation)
-        return venations
-class Particle:
-    """
-    This object represent a Particle of light
-
-    attributes:
-        coord: currant position of the particle
-        last_venation: VenationPoint object, corresponding to the last position of the particle
-        dir_to_p: the direction to go to the petiole
-    """
-    dir_to_p = None
-    coord = None
-    last_venation = None
-
-    def __init__(self, coord):
-        self.coord = coord
-
-    def display_particle(self):
-        plot(self.coord[0], self.coord[1], 'ro')
-
-    def move(self):
-        self.coord = [self.coord[0] + 0.3 * self.dir_to_p[0], self.coord[1] + 0.1 * self.dir_to_p[1]]
-        self.last_venation = VenationPoint(self.coord, [self.last_venation])
 
 
 class VenationPoint:
