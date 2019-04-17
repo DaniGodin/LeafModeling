13e332a6c7ac74f7f6fc393efaae4d0c5acc51bb
Daniel Godin <danielgodin.pro@gmail.com>
2019-04-17 22:16:18 +0200

    created class for auxin growth method

--- AlgoLeaf/auxin_growth/main.py
+++ AlgoLeaf/auxin_growth/main.py
@@ -1,3 +1,16 @@
 
 
 
+if __name__ == '_main_':
+    """
+    step 1: get a shape with petiole position
+    
+    step 2: projet auxin on the leaf (kill distance)
+    
+    step 3: move one veinnode 
+    
+    
+    
+    
+"""
+
