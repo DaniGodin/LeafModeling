9c25c759200466998afdc91245876d36eff036fe
Hadrien Delassus <hadrien.del@protonmail.com>
2019-04-25 20:58:34 +0200

    [Examples] Examples source file containing all the examples

---
+++ Cpp/src/Examples.hh
@@ -0,0 +1,21 @@
+//
+// Created by revetoon on 4/25/19.
+//
+
+#ifndef OBJPARSER_EXAMPLES_HH
+#define OBJPARSER_EXAMPLES_HH
+
+
+namespace Examples {
+
+    void parseFileExample();
+    void leafGrowthExample();
+    void treeExample();
+    void cylinderExample();
+    void algoLeafExample();
+    void parametricExample();
+
+}
+
+
+#endif //OBJPARSER_EXAMPLES_HH
