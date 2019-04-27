6466536899abc580a998c4914e4302cb9ee66c0c
Daniel Godin <danielgodin.pro@gmail.com>
2019-04-27 22:30:07 +0200

    [LEAF] now can gen randm into complicated shapes

--- AlgoLeaf/auxin_growth/leaf_shape.py
+++ AlgoLeaf/auxin_growth/leaf_shape.py
@@ -1,48 +1,63 @@
 import random as rand
 
 from pylab import *
+import matplotlib.patches as patches
 
 class first_param:
 
     growth_size = 0
 
     equation = None
+    rectangle = None #where to project the points
+    ax = None
 
     points = []
 
 
-    def __init__(self, equation, _growth):
+    def __init__(self, equation, _growth, rectangle):
         self.equation = equation
         self.growth_size = _growth
+        self.rectangle = rectangle
+        fig, self.ax = plt.subplots()
 
 
-    def plot_shape(self, _growth):
-        self.gen_points(1)
-        self.plot_points()
-        self.growth_size = _growth
+    def plot_shape(self):
         x = np.linspace(-1.0, 1.0, 1000)
         y = np.linspace(-0.5, 1.0, 1000)
         X, Y = np.meshgrid(x, y)
         F = -((self.growth_size * X) ** 2 + (self.growth_size * Y - 1) ** 2 - 1) ** 3 - \
             ((self.growth_size * X) ** 2) * ((self.growth_size * Y - 1) ** 3)
-        plt.contour(X, Y, F, [0])
-        plt.show()
+        self.ax.contour(X, Y, F, [0])
+        x = self.rectangle[0][0]
+        y = self.rectangle[0][1]
+        x_lim = x + self.rectangle[1]
+        y_lim = y + self.rectangle[2]
+
+        rect = patches.Rectangle((x, y), x_lim, y_lim, linewidth=1, edgecolor='r', facecolor='none')
+        self.ax.add_patch(rect)
+        show()
 
 
-    def plot_points(self):
-        plot(self.points[0][0], self.points[0][1], 'bo')
 
-    def gen_points(self, nb_points):
+    def plot_points(self, coord):
+        plot(coord[0], coord[1], 'bo')
+
+    def gen_point(self):
+        coord = None
         find = False
+        x = self.rectangle[0][0]
+        y = self.rectangle[0][1]
+        x_lim = x + self.rectangle[1]
+        y_lim = y + self.rectangle[2]
         while not find:
-            r1 = random()
-            r2 = random()
+            r1 = x + random() * (x_lim - x)
+            r2 = y + random() * (y_lim - y)
 
             if (-((self.growth_size * r1) ** 2 + (self.growth_size * r2 - 1) ** 2 - 1) ** 3 - \
             ((self.growth_size * r1) ** 2) * ((self.growth_size * r2 - 1) ** 3)) >= 0:
                 find = True
-                self.points.append([r1, r2])
-
+                coord = [r1, r2]
+        return coord
 
 
 
--- AlgoLeaf/auxin_growth/main.py
+++ AlgoLeaf/auxin_growth/main.py
@@ -6,17 +6,22 @@ from pylab import *
 if __name__ == '__main__':
 
 
+    """
 
-
-    leaf_shape = Polygon([(-1, 0), (0, 3), (1, 0)], closed=True)
+    leaf_shape = Polygon([(-1, 0), (0, 3), (1, 0)], closed=True)    
 
     Bob = Leaf(0.5, 0.6, leaf_shape,150, 4, 0.004)
 
     Bob.run_creation(1300)
 
     Bob.display(8, 15)
+    """
 
 
 
-    #Leaf = first_param(None, 2.8)
-   # Leaf.plot_shape(2.8)
+    Leaf = first_param(None, 6, ([-0.7, -0.2], 2, 1.4))
+    point = Leaf.gen_point()
+    Leaf.plot_points(point)
+    Leaf.plot_shape()
+
+
