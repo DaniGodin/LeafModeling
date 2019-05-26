aa623e83403fb002dac6412ce08a255e750bf570
Daniel Godin <danielgodin.pro@gmail.com>
2019-05-26 02:05:24 +0200

    [CPP] leaf_shape class implemented

--- Cpp/src/AlgoLeaf_Auxin/Leafshape.cpp
+++ Cpp/src/AlgoLeaf_Auxin/Leafshape.cpp
@@ -3,3 +3,46 @@
 //
 
 #include "Leafshape.hh"
+#include <random>
+
+
+namespace Shape{
+
+
+    Point3D Leafshape::gen_point() {
+
+        bool find = false;
+        Point3D coord;
+
+        double x = r.origin.getX();
+        double y = r.origin.getY();
+
+        double x_lim = x + r.x_lim;
+        double y_lim = y + r.y_lim;
+
+
+        while(!find){
+
+            std::uniform_real_distribution<double> unif(0,1);
+            std::default_random_engine re;
+            double r1 = x + unif(re) * ( x_lim - x);
+            double r2 = y + unif(re) * ( y_lim - y);
+            Point3D rand = Point3D(r1, r2, 0);
+            double eval_point = equation(rand, growth_size);
+
+            if (0.05 >= eval_point && eval_point >= 0.02){
+
+                coord = rand;
+                find = true;
+
+            }
+
+        }
+
+        return coord;
+
+    }
+
+
+
+}
--- Cpp/src/AlgoLeaf_Auxin/Leafshape.hh
+++ Cpp/src/AlgoLeaf_Auxin/Leafshape.hh
@@ -6,9 +6,42 @@
 #define ALGOLEAF_AUXIN_LEAFSHAPE_HH
 
 
-class Leafshape {
+#include "../Obj/Point3D.hh"
+#include "functional"
 
-};
 
 
+
+namespace  Shape {
+
+
+    struct rectangle {
+
+        Point3D origin;
+        double x_lim;
+        double y_lim;
+
+    };
+
+    class Leafshape {
+
+
+    public:
+
+        double growth_size = 0;
+        rectangle r; // where to project points
+        std::function<double(Point3D, double)> equation;
+
+    public:
+
+        Leafshape(double _growth, std::function<double(Point3D, double)> _equ, rectangle _r) : growth_size(_growth),
+                                                                                               r(_r),
+                                                                                               equation(_equ) {};
+
+        Point3D gen_point();
+
+
+    };
+
+}
 #endif //ALGOLEAF_AUXIN_LEAFSHAPE_HH
